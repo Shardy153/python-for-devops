878563d5a0aafb4493b73116f41edf79c9abb414
Vipul Sharda <45224524+Shardy153@users.noreply.github.com>
2025-05-28 17:27:10 +0530

    Update ssh-paramiko.py

--- ssh-paramiko.py
+++ ssh-paramiko.py
@@ -1,16 +1,23 @@
 import paramiko
 
 def ssh_server(hostname, username, command):
-	client = paramiko.SSHClient()
-	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	client.connect(hostname=hostname, username=username)
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
+    # Connect without a password or key – consider adding password or key support if needed
+    client.connect(hostname=hostname, username=username)
+    
+    stdin, stdout, stderr = client.exec_command(command)
+    
+    # Print stdout lines and stderr output
+    print("Output:\n", ''.join(stdout.readlines()))
+    print("Errors:\n", ''.join(stderr.readlines()))
+    
+    client.close()
 
-	stdin, stdout, stderr = client.exec_command(command)
-	print(stdout.readlines(), stderr.readlines())
-	client.close()
+# Input from user
+hostname = input("Enter hostname: ")
+username = input("Enter username: ")
+command = input("Enter command to execute: ")
 
-
-hostname = input("Enter hostname : ")
-username = input("Enter username : ")
-command = input("Enter command to execute : ")
 ssh_server(hostname, username, command)
